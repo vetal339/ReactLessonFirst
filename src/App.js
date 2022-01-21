a3e338a49bda76f567d5cd15522e8ccc2b2eb8b1
Vetal339 <vitalijkadylinskij@gmail.com>
2022-01-21 14:14:32 +0300

    create comment structure and added post or deleted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/app.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'Description'},
+        {id: 2, title: 'JavaScript 2', body: 'Description'},
+        {id: 3, title: 'JavaScript 3', body: 'Description'}
+    ])
+   
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form>
+            <input type="text" placeholder="Название поста" />
+            <input type="text" placeholder="Описание поста" />
+            <MyButton>
+                Создать пост
+            </MyButton>
+        </form>
+        <PostList posts={posts} title={'Посты про JS'} />
     </div>
   );
 }
