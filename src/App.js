ce1a1f35cfcc3e5691828f3efc6546b9b96ece46
Vetal339 <vitalijkadylinskij@gmail.com>
2022-01-24 17:08:11 +0300

    creat limit page and button for page,and more create change page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
 
 
 
@@ -22,10 +23,18 @@ function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false);
+    const [totalPages, setTotalPages] = useState(0);
+    const[limit, setLimit] = useState(10);
+    const[page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+    let pagesArray = getPagesArray(totalPages);
+
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () =>{
-        const posts = await PostService.getAll();
-        setPosts(posts)
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
     })
 
     useEffect(()=> {
@@ -38,6 +47,10 @@ function App() {
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
+    }
+    const changePage = (page) => {
+        setPage(page);
+        fetchPosts();
     }
 
 
@@ -63,6 +76,17 @@ function App() {
             ?  <div style={{display: 'flex', justifyContent: 'center', marginTop: '150px'}}><Loader /></div>
             :  <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'} />
         }
+        <div className="page__wrapper">
+            {pagesArray.map(p =>
+                <span
+                    onClick={() => changePage(p)}
+                    key={p}
+                    className={page === p ? 'page page__current' : 'page'}
+                >
+                    {p}
+                </span>
+            )}
+        </div>
     </div>
   );
 }
